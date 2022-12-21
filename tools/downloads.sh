80a08155b8a4e954d34f6f38fd83c92a63e88d14
morningman <morningman@163.com>
2022-12-21 11:33:51 +0800

    add downloads

---
+++ tools/downloads.sh
@@ -0,0 +1,4 @@
+wget https://github.com/amosbird/ldb_toolchain_gen/releases/download/v0.14.2/ldb_toolchain_gen.sh
+wget https://doris-thirdparty-hk-1308700295.cos.ap-hongkong.myqcloud.com/tools/apache-maven-3.6.3-bin.tar.gz
+wget https://doris-thirdparty-hk-1308700295.cos.ap-hongkong.myqcloud.com/tools/jdk-8u131-linux-x64.tar.gz
+wget https://doris-thirdparty-hk-1308700295.cos.ap-hongkong.myqcloud.com/tools/node-v12.13.0-linux-x64.tar.gz
